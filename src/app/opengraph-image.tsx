8ffd91fe3362e61680e281572c9645dc190cbe33
Emirsyah <muhammademir48@gmail.com>
2026-02-17 01:51:57 +0700

    feat(seo): configure metadata, sitemap, and open graph

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,70 @@
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "Open TA Telyu";
+export const size = {
+    width: 1200,
+    height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image() {
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 128,
+                    background: "white",
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    fontFamily: "sans-serif",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        gap: "20px",
+                    }}
+                >
+                    <div
+                        style={{
+                            fontSize: 64,
+                            fontWeight: 900,
+                            letterSpacing: "-0.05em",
+                        }}
+                    >
+                        Open TA Telyu
+                    </div>
+                    <div
+                        style={{
+                            fontSize: 32,
+                            fontWeight: 400,
+                            color: "#666",
+                        }}
+                    >
+                        Telkom University Alumni Research Repository
+                    </div>
+                </div>
+            </div>
+        ),
+        // ImageResponse options
+        {
+            // For convenience, we can re-use the exported opengraph-image
+            // size config to also set the ImageResponse's width and height.
+            ...size,
+        }
+    );
+}
