d7ccef198df903a78ee2f7085a0546210d73ae61
Emirsyah <muhammademir48@gmail.com>
2026-04-09 17:05:07 +0700

    chore: remove maintanance banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,7 +107,7 @@ export default function RootLayout({
           <SidebarProvider className="h-svh overflow-hidden">
             <AppSidebar />
             <SidebarInset className="overflow-hidden">
-              <MaintenanceBanner />
+              {/* <MaintenanceBanner /> */}
               <header className="flex h-14 shrink-0 items-center gap-2 border-b px-4">
                 <SidebarTrigger className="-ml-1" />
                 <Separator orientation="vertical" className="h-6" />
